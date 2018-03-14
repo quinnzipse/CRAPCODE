68383400045679364b855a1800f95d624528902f
Quinn Zipse <qzipse@outlook.com>
2018-03-14 09:13:16 -0500

    Changed the code to comply with the pink box

--- src/org/usfirst/frc/team6758/robot/Robot.java
+++ src/org/usfirst/frc/team6758/robot/Robot.java
@@ -7,78 +7,65 @@
 
 package org.usfirst.frc.team6758.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
- * project.
- */
 public class Robot extends IterativeRobot {
-	Spark frontLeft = new Spark(0);
+/*	Spark frontLeft = new Spark(0);
 	Spark frontRight = new Spark(1);
 	Spark backLeft = new Spark(2);
-	Spark backRight = new Spark(3);
+	Spark backRight = new Spark(3);*/
 	
-	MecanumDrive driveTrain = new MecanumDrive(frontLeft, backLeft, frontRight, backRight);
+	WPI_TalonSRX frontRight = new WPI_TalonSRX(0);
+	WPI_TalonSRX backRight = new WPI_TalonSRX(1);
+	WPI_TalonSRX frontLeft = new WPI_TalonSRX(2);
+	WPI_TalonSRX backLeft = new WPI_TalonSRX(3);
 	
+	public SpeedControllerGroup left = new SpeedControllerGroup(frontLeft, backLeft);
+	public SpeedControllerGroup right = new SpeedControllerGroup(frontRight, backRight);
+	
+	DifferentialDrive driveTrain = new DifferentialDrive(left, right);
 	Joystick stick = new Joystick(0);
-	
 	Compressor compressor = new Compressor();
-
 	Solenoid solenoid = new Solenoid(0);
 	
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
+	private boolean safe = true;
+	private double mainSpeed = .8, twistSpeed = .65;
+	
 	@Override
 	public void robotInit() {
 		compressor.setClosedLoopControl(true);
 	}
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * <p>You can add additional auto modes by adding additional comparisons to
-	 * the switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
 	@Override
 	public void autonomousInit() {
 	}
 
-	/**
-	 * This function is called periodically during autonomous.
-	 */
 	@Override
 	public void autonomousPeriodic() {
 	}
 
-	/**
-	 * This function is called periodically during operator control.
-	 */
 	@Override
 	public void teleopPeriodic() {
-		driveTrain.driveCartesian(stick.getY(), stick.getX(), stick.getTwist());
+		driveTrain.arcadeDrive(stick.getY()*mainSpeed, stick.getTwist()*twistSpeed);
 		
-		solenoid.set(stick.getTrigger());
+		RobotController.getUserButton();
+		
+		if(stick.getRawButtonPressed(2)) {
+			if(safe) safe = false;
+			else safe = true;
+		}
+		
+		solenoid.set(stick.getTrigger() && safe);
 	}
 
-	/**
-	 * This function is called periodically during test mode.
-	 */
 	@Override
 	public void testPeriodic() {
 	}
